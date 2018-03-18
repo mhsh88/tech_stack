702a8c186dbbc53c1db912922080d082ef14cc81
hossein sharifi <hosseinsharifi@hotmail.com>
2018-03-18 16:26:25 +0330

    add header and other component to provider

---
+++ src/components/common/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 50,
+    elevation: 5
+  },
+  textStyle: {
+      fontSize: 20
+    }
+};
+
+
+export { Header };
