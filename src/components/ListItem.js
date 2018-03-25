4aea57a992b4c43c47f8c131eec41bd13e96f634
hossein sharifi <hosseinsharifi@hotmail.com>
2018-03-25 13:26:53 +0430

    add some animation and make it possible for android

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import {
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  LayoutAnimation,
+  UIManager,
+  Platform
+  } from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+  constructor() {
+ super();
+ if (Platform.OS === 'android') {
+     UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
+ }
+}
+  componentWillUpdate() {
+    LayoutAnimation.spring();
+  }
   renderDescription() {
     const { library, expanded } = this.props;
 
     if (expanded) {
       return (
-        <Text>{library.description}</Text>
+        <CardSection>
+          <Text style={{ flex: 1 }}>{library.description}</Text>
+        </CardSection>
       );
     }
   }
